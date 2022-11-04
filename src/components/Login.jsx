2c774c62b8b73547369b0960d3a8d9f454101d75
Tyler Jefferson <tylerrjefferson00@gmail.com>
2022-11-04 15:52:21 -0400

    profile page, tokens, etc

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,8 +1,18 @@
 import { Button, Form, Input, Layout } from "antd"
 
-export default function Login() {
+export default function Login({ setUser, setToken }) {
   const handleFormSubmit = (values) => {
-    console.log("Success:", values)
+    fetch('http://localhost:3030/login', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(values)
+    })
+      .then(response => response.json())
+      .then(data => {
+        setToken(data.token)
+        setUser(data.user)
+      })
+      .catch(alert)
   }
   return (
   <Layout.Content style={{ padding: '50px' }}>
@@ -17,7 +27,11 @@ export default function Login() {
       <Form.Item label="Password" name='password' rules={[{
         required: true,
         message: "Please enter password"
-      }]}>
+      }, {
+        min: 8,
+        message: 'Password must be at least 8 characters'
+      }]
+      }>
         <Input.Password />
       </Form.Item>
       <Form.Item wrapperCol={{ span: 16, offset: 8 }}>
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,3 +1,38 @@
-export default function Profile() {
-  return <h1>Profile</h1>
+import { Button, Form, Input, Layout } from 'antd';
+
+export default function Profile({ user, token, setUser }) {
+  const handleProfileUpdate = (values) => {
+    // send form values and token to api
+    fetch(`http://localhost:3030/users/${user.uid}`, {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization' : token,
+      },
+      body: JSON.stringify(values),
+    })
+    .then(response => response.json())
+    .then(setUser)
+    .catch(alert)
+  }
+  return (
+    <Layout.Content style={{ padding: '50px' }}>
+      <h1>Profile</h1>
+      <Form 
+      onFinish={handleProfileUpdate}
+      initialValues={user}
+      labelCol={{ span: 8}} 
+      wrapperCol={{ span: 16}}>
+        <Form.Item label="Name" name="name">
+          <Input />
+        </Form.Item>
+        <Form.Item label="Email" name="email">
+          <Input />
+        </Form.Item>
+        <Form.Item wrapperCol={{ span: 16, offset: 8 }}>
+          <Button type='primary' htmlType='submit'>Save</Button>
+        </Form.Item>
+      </Form>
+    </Layout.Content>
+  )
 }
